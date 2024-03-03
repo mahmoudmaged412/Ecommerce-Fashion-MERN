d9c4f7e5d816c13e2c18610c32713bba210ff898
Mahmoud Tourky <mahmoudmaged412@gmail.com>
2024-03-03 21:00:49 +0100

    GET ALL USERS

--- api/routes/user.js
+++ api/routes/user.js
@@ -54,4 +54,23 @@ router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
+//GET ALL USERS
+
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  const query = req.query.new;
+  try {
+    const users = query
+      ? await User.find().sort({ _id: -1 }).limit(1)
+      : await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+//GET USER STATS
+
+
+
 module.exports = router;
