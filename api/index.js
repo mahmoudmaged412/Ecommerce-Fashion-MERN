f75894416915d880a44f64e984197a37e5ec22f4
Mahmoud Tourky <mahmoudmaged412@gmail.com>
2024-03-03 21:34:06 +0100

    Adding product

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL)
@@ -14,6 +15,8 @@ mongoose.connect(process.env.MONGO_URL)
 app.use(express.json());
 app.use("/api/auth",authRoute);
 app.use("/api/users",userRoute);
+app.use("/api/products", productRoute);
+
 
 
   app.listen(5000, ()=>{
--- api/routes/product.js
+++ api/routes/product.js
@@ -1,3 +1,39 @@
-const router =require("express").Router();
+const Product = require("../models/Product");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
 
-module.exports = router
+const router = require("express").Router();
+
+//CREATE
+
+router.post("/", verifyTokenAndAdmin, async (req, res) => {
+  const newProduct = new Product(req.body);
+
+  try {
+    const savedProduct = await newProduct.save();
+    res.status(200).json(savedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//UPDATE
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
