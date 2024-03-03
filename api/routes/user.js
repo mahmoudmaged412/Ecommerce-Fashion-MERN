39e2bf8624352aee0b5581a2bcaeba3ebb9c1327
Mahmoud Tourky <mahmoudmaged412@gmail.com>
2024-03-03 18:31:04 +0100

    building-verifytoken-functions

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,3 +1,21 @@
+const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+
 const router =require("express").Router();
 
-module.exports = router
+//Update 
+
+router.put("/:id",verifyTokenAndAuthorization,(req,res)=>{
+    if(req.body.password){
+        req.body.password= CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.PASS_SEC
+          ).toString();
+    }
+    try{
+
+    }catch(err){
+        console.log(first)
+    }
+});
+
+module.exports = router;
