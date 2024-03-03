0ed2a794d695287837ede29ee4a76d7e13eb7063
Mahmoud Tourky <mahmoudmaged412@gmail.com>
2024-03-03 18:11:29 +0100

    database-connection

---
+++ api/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app =express();
+const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Failed to connect to MongoDB", err));
+
+
+app.use(express.json());
+app.use("/api/auth",authRoute);
+app.use("/api/users",userRoute);
+
+
+  app.listen(5000, ()=>{
+    console.log("server is running")
+});
