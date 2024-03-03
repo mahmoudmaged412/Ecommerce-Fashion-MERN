d22c7169aeba962c5188e3768fe404771a01e53c
Mahmoud Tourky <mahmoudmaged412@gmail.com>
2024-03-03 22:09:57 +0100

    order finish

--- api/routes/order.js
+++ api/routes/order.js
@@ -46,7 +46,7 @@ router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
-//GET USER ORDERS
+// GET USER ORDERS
 router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
   try {
     const orders = await Order.find({ userId: req.params.userId });
