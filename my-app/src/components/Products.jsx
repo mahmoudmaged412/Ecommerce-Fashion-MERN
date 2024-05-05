d546c1ebcff22f2d69989029e7865cf4d3bb3d53
Mahmoud Tourky <mahmoudmaged412@gmail.com>
2024-05-06 00:35:45 +0200

    continue redux building

--- my-app/src/components/Products.jsx
+++ my-app/src/components/Products.jsx
@@ -1,6 +1,8 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { popularProducts } from "../data";
 import Product from "./Product";
+import axios from "axios";
 
 const Container = styled.div`
   padding: 20px;
@@ -8,11 +10,13 @@ const Container = styled.div`
   flex-wrap: wrap;
   justify-content: space-between;
 `;
+
+
 const Products = () => {
   return (
     <Container>
-      {popularProducts.map((item) => (
-        <Product item={item} key={item.id} />
+      {popularProducts.map((item)=>(
+        <Product item ={item} key={item.id}/>
       ))}
     </Container>
   );
